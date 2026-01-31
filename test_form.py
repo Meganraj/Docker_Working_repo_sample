c76f69a6ecd7a86288d3331e61b782035b6932f7
Meganraj <meganrajmegan@gmail.com>
2026-01-31 17:13:37 +0530

    Modified the Code

--- test_form.py
+++ test_form.py
@@ -4,20 +4,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
-# ---------------------------
+print("🚀 Selenium script started")
+
 # Chrome options for Docker
-# ---------------------------
 chrome_options = Options()
-chrome_options.add_argument("--headless=new")       # Mandatory for Docker
-chrome_options.add_argument("--no-sandbox")         # Mandatory for Docker
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1920,1080")
 
-# Path to chromedriver inside container
+# Explicit chromedriver path
 service = Service("/usr/bin/chromedriver")
 
-# Start browser
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 try:
@@ -53,4 +52,4 @@ except Exception as e:
 
 finally:
     driver.quit()
-    print("Browser closed")
+    print("🏁 Selenium script finished")
